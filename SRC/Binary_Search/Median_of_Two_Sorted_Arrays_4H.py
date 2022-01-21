4e300747c56563569c0dd6725bd30f5b0208f388
Xiaoqiang Xu <55418642+Xiaoqiang-Xu@users.noreply.github.com>
2022-01-21 17:43:52 -0500

    Update Median_of_Two_Sorted_Arrays_4H.py

--- SRC/Binary_Search/Median_of_Two_Sorted_Arrays_4H.py
+++ SRC/Binary_Search/Median_of_Two_Sorted_Arrays_4H.py
@@ -32,3 +32,42 @@ class Solution:
                 
         return -1
                 
+    #Solution on 1.21.2022
+   class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        
+        m = len(nums1)
+        n = len(nums2)
+        
+        if m > n:
+            return self.findMedianSortedArrays(nums2, nums1)
+        
+        cut1, cut2 = 0, 0
+        L1, R1 = 0, 0
+        L2, R2 = 0, 0
+        
+        start, end = 0, m 
+        while True:
+            cut1 = (start + end) // 2
+            cut2 = (m + n) // 2 - cut1
+            L1 = nums1[cut1 - 1] if cut1 != 0 else float('-inf')
+            R1 = nums1[cut1] if cut1 != m  else float('inf')
+            L2 = nums2[cut2 - 1] if cut2 != 0 else float('-inf')
+            R2 = nums2[cut2] if cut2 != n  else float('inf')
+            if L1 > R2:
+                end = cut1 - 1
+            elif L2 > R1:
+                start = cut1 + 1
+            else:
+                if (m + n) % 2 == 0:
+                    L1 = max(L1, L2)
+                    R1 = min(R1, R2)
+                    return (L1 + R1) / 2
+                else:
+                    R1 = min(R1, R2)
+                    return R1
+        
+            
+        
+        
+        
